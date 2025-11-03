ea49489c966cb70d6ef7de0b989819cf2b46133a
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-11-04 00:06:54 +0600

    Create Banner, Load books data and show all the books

---
+++ src/Pages/Books/Books.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+const Books = () => {
+    const [allBooks, setAllBooks] = useState([]);
+
+    useEffect(()=>{
+        fetch('booksData.json')
+        .then(rse => rse.json())
+        .then(data => {
+           setAllBooks(data)
+        })
+    },[])
+    return (
+        <div>
+            <h1>hello ami books</h1>
+        </div>
+    );
+};
+
+export default Books;
