2ce19d8d2680939612c593023df1510552490dee
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-11-30 23:59:23 +0600

    Load book details page and display

--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,18 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
+import Book from '../Book/Book';
 
-const Books = () => {
+const Books = ({data}) => {
     const [allBooks, setAllBooks] = useState([]);
 
-    useEffect(()=>{
-        fetch('booksData.json')
-        .then(rse => rse.json())
-        .then(data => {
-           setAllBooks(data)
-        })
-    },[])
+    // useEffect(()=>{
+    //     fetch('booksData.json')
+    //     .then(rse => rse.json())
+    //     .then(data => {
+    //        setAllBooks(data)
+    //     })
+    // },[])
+
+    // const bookPromise = fetch('./booksData.json').then(res => res.json())
+
     return (
         <div>
-            <h1>hello ami books</h1>
+            <h1 className='text-3xl text-center p-6'> Books</h1>
+            <Suspense fallback= {<span>Loading.....</span>}>
+           {
+            data.map((singleBook)=><Book key={singleBook.bookId} singleBook={singleBook} ></Book>)
+           }
+            </Suspense>
         </div>
     );
 };
