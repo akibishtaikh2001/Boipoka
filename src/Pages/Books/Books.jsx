109aa25beec89d4c696789f330bff3df9b1ee841
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-12-01 00:24:19 +0600

    Load book details page and display Update

--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense, useEffect, useState } from 'react';
 import Book from '../Book/Book';
 
-const Books = ({data}) => {
+const Books = ({ data }) => {
     const [allBooks, setAllBooks] = useState([]);
 
     // useEffect(()=>{
@@ -17,14 +17,15 @@ const Books = ({data}) => {
     return (
         <div>
             <h1 className='text-3xl text-center p-6'> Books</h1>
-            <Suspense fallback= {<span>Loading.....</span>}>
-           {
-            data.map((singleBook)=><Book key={singleBook.bookId} singleBook={singleBook} ></Book>)
-           }
+            <Suspense fallback={<span>Loading.....</span>}>
+                <div className=' grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3  '>
+                    {
+                        data.map((singleBook) => <Book key={singleBook.bookId} singleBook={singleBook} ></Book>)
+                    }
+                </div>
             </Suspense>
         </div>
     );
 };
 
-export default Books;        
-               
+export default Books;
